933db9f491d4ff7adabe57316f5bb826bb069556
Aleksei <60549770+Daiver500@users.noreply.github.com>
2021-08-20 22:27:18 +0300

    modal window done

--- webpack.config.js
+++ webpack.config.js
@@ -2,21 +2,19 @@ const path = require("path");
 const miniCss = require("mini-css-extract-plugin");
 
 module.exports = {
+
   entry: [
-    "./js/main.js",
-    "./js/modal.js",
-    "./js/slider.js",
     "./js/index.js",
+    "./js/modal.js",
   ],
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname),
     iife: true
   },
-  module: {
+   module: {
     rules: [
       { test: /\.(s*)css$/, use: [ miniCss.loader, "css-loader?url=false", "sass-loader" ] },
-      { test: /\.svg$/, use: "svg-inline-loader" },
     ],
  },
  plugins: [
@@ -24,7 +22,8 @@ module.exports = {
        filename: "style.css",
     }),
  ],
-  devtool: false
+   devtool: false,
 };
+
 
 
