c4d550156b40dc8ac0ea7add292330c7a57c4dfd
Aleksei <60549770+Daiver500@users.noreply.github.com>
2021-08-21 01:27:59 +0300

    public

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   ],
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname),
+    path: path.resolve(__dirname, "public"),
     iife: true
   },
    module: {
